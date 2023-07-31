2207edac76997175d0b622c7fb04b07e53b06758
Vinicius Vaz <agvazvinicius@gmail.com>
2023-07-31 15:00:41 -0300

    [update] files

---
+++ js/weather.js
@@ -0,0 +1,26 @@
+const APIkey = `	EyrzD3XtLqm4XbLsT7kBW0ofGzlPMP7d`;
+const baseUrl = `http://dataservice.accuweather.com/`
+
+
+const getCityUrl = (cityName) =>
+   `${baseUrl}locations/v1/cities/search?apikey=${APIkey}&q=${cityName}`;
+
+const getWheatherUrl = cityKey => `${baseUrl}currentconditions/v1/${cityKey}?apikey=${APIkey}&language=pt-br`
+
+
+const fetchData = async url => {
+   try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+         throw new Error("Não foi possivel obter os dados");
+      }
+
+      return response.json();
+   } catch ({ name, message }) {
+      alert(`${name}: ${message}`);
+   }
+};
+
+const getCityData = (cityName) => fetchData(getCityUrl(cityName));
+const getCityWeather = cityKey => fetchData(getWheatherUrl(cityKey))
