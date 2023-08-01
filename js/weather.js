26e548c1270cce6382fd034293763cb12873da5e
Vinicius Vaz <agvazvinicius@gmail.com>
2023-08-01 13:52:14 -0300

    [update] country fetch to pt-br

--- js/weather.js
+++ js/weather.js
@@ -2,7 +2,7 @@ const APIkey = `YJZoshry97akAGCruOWwohJd04cgwcPE`;
 const baseUrl = `https://dataservice.accuweather.com/`;
 
 const getCityUrl = (cityName) =>
-	`${baseUrl}locations/v1/cities/search?apikey=${APIkey}&q=${cityName}`;
+	`${baseUrl}locations/v1/cities/search?apikey=${APIkey}&q=${cityName}&language=pt-br`;
 
 const getWheatherUrl = (cityKey) =>
 	`${baseUrl}currentconditions/v1/${cityKey}?apikey=${APIkey}&language=pt-br`;
