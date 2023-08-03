3a063cc24c901d737790cd21c4619315e548e93f
Vinicius Vaz <agvazvinicius@gmail.com>
2023-08-03 16:05:58 -0300

    [update] initial data management

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const cityForm = document.querySelector('[data-js="change-location"]');
 const cityNameContainer = document.querySelector('[data-js="city-name"]');
-const countryNameContainer = document.querySelector('[data-js="country-name"]')
+const countryNameContainer = document.querySelector('[data-js="country-name"]');
 const cityWeatherContainer = document.querySelector('[data-js="city-weather"]');
 const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]');
 const cityCard = document.querySelector('[data-js="city-card"]');
@@ -8,30 +8,50 @@ let timeImg = document.querySelector('[data-js="time"]');
 const timeIconContainer = document.querySelector('[data-js="time-icon"]');
 
 const showCard = () => {
-   if (cityCard.classList.contains("d-none")) {
-      cityCard.classList.remove("d-none");
-   }
+	if (cityCard.classList.contains("d-none")) {
+		cityCard.classList.remove("d-none");
+	}
 };
 
 const showCityWeatherInfo = async (cityName) => {
-   const [{ Key, LocalizedName, Country, AdministrativeArea
-   }] = await getCityData(cityName);
-   const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key);
-   const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg">`;
+	const [{ Key, LocalizedName, Country, AdministrativeArea }] = await getCityData(cityName);
+	const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key);
+	const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg">`;
 
-   timeImg.src = IsDayTime ? "./src/day.svg" : "./src/night.svg";
-   timeIconContainer.innerHTML = timeIcon;
-   cityNameContainer.textContent = `${LocalizedName} - ${AdministrativeArea.ID}`;
-   countryNameContainer.textContent = `${Country.LocalizedName}`
-   cityWeatherContainer.textContent = WeatherText;
-   cityTemperatureContainer.textContent = Temperature.Metric.Value.toFixed(0);
+	timeImg.src = IsDayTime ? "./src/day.svg" : "./src/night.svg";
+	timeIconContainer.innerHTML = timeIcon;
+	cityNameContainer.textContent = `${LocalizedName} - ${AdministrativeArea.ID}`;
+	countryNameContainer.textContent = `${Country.LocalizedName}`;
+	cityWeatherContainer.textContent = WeatherText;
+	cityTemperatureContainer.textContent = Temperature.Metric.Value.toFixed(0);
+
+	Storage.set(cityName);
 };
 
 cityForm.addEventListener("submit", (event) => {
-   event.preventDefault();
-   const inputValue = event.target.city.value;
+	event.preventDefault();
+	const inputValue = event.target.city.value;
 
-   showCard();
-   showCityWeatherInfo(inputValue);
-   cityForm.reset();
+	showCard();
+	showCityWeatherInfo(inputValue);
+	cityForm.reset();
+});
+
+const Storage = {
+	get() {
+		return JSON.parse(localStorage.getItem("city")) || '';
+	},
+
+	set(cityName) {
+		localStorage.setItem("city", JSON.stringify(cityName));
+	},
+};
+
+window.addEventListener("load", () => {
+   let storagedCity = Storage.get()
+
+   if(storagedCity) {
+      showCard()
+      showCityWeatherInfo(Storage.get());
+   }
 });
